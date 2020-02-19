077f3e3f7b1a1b59c7cebd52786a88ef8e4772a0
Kelly <kellydekitani@gmail.com>
2020-02-19 13:26:32 -0800

    added in dependencies for AWS

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
 const PORT = 3000;
+const fileUpload = require('express-fileupload')
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(fileUpload());
 app.get('*', (req, res) => {
   res.sendStatus(404);
 });
