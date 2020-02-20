68291da96bfaedce963f575f55b6509ff2fd5d51
Kelly <kellydekitani@gmail.com>
2020-02-19 16:49:56 -0800

    can add multiple images

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -8,6 +8,22 @@ cloudinary.config({
   api_secret: 'QSaAUgEMAaUhC-Jl5q6oIqk9SYI' 
 });
 
+itemController.addImage = (req, res, next) => {
+  const values = Object.values(req.files)
+  // console.log(values)
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  Promise
+    .all(promises)
+    .then(results => {
+    // console.log('results url' , results[0].url)
+      res.locals.imgURL = results[0].url 
+      return next();
+    })
+    .catch(err => {
+      console.log(err);
+      })
+}
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -5,11 +5,28 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const cloudinary = require('cloudinary').v2;
+const formData = require('express-form-data')
+const itemController = require('./controllers/itemController');
 
+// cloudinary.config({ 
+//   cloud_name: 'swapme', 
+//   api_key: '664311791237882', 
+//   api_secret: 'QSaAUgEMAaUhC-Jl5q6oIqk9SYI' 
+// });
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(formData.parse())
+
+
+//post request to add images 
+app.post('/uploadImage', itemController.addImage, (req, res) =>{
+  res.status(200).json(res.locals.imgURL);
+})
+
+
+
 
 app.get('*', (req, res) => {
   res.sendStatus(404);
