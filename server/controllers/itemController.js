dd9a79c89709e38ff6211d6a7146f7d9a9497a3b
Kelly <kellydekitani@gmail.com>
2020-02-20 09:23:11 -0800

    updated image upload functionality

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -8,7 +8,42 @@ cloudinary.config({
   api_secret: 'QSaAUgEMAaUhC-Jl5q6oIqk9SYI' 
 });
 
+itemController.saveImage = async (req, res, next) =>{
+  const {item_id}= req.params;
+  const {user_id, imgURL} = res.locals
+  const text = `INSERT image(img_url, item_id, user_id)
+                VALUES ($1, $2, $3)
+                RETURNING *`
+  const values = [imgURL, item_id, user_id]
+  await db.query(text, values)
+  .then(response => {
+    console.log(`this is the response in saveImage:`, response)
+    next();
+  })
+
+}
+
+//ANOTHER MIDDLEWARE TO GET THE USER_ID FROM THE ITEM TABLE 
+itemController.getUserId= async (req,res, next) =>{
+  const {item_id} = req.params;
+  const text = `SELECT user_id FROM item i WHERE i.item_id = $1 RETURNING user_id`
+  const values = [item_id];
+  await db.query(text, values)
+  .then(response => {
+    console.log('this is the response in getUserId:',response);
+    res.locals.user_id = response.rows[0];
+    next();
+  })
+  .catch(err =>{
+    return next(err);
+  })
+}
+
+//adding the image to cloudinary 
 itemController.addImage = (req, res, next) => {
+  // const image = req.files;
+  // console.log('image:', image)
+  // return next();
   const values = Object.values(req.files)
   // console.log(values)
   const promises = values.map(image => cloudinary.uploader.upload(image.path))
@@ -17,11 +52,16 @@ itemController.addImage = (req, res, next) => {
     .then(results => {
     // console.log('results url' , results[0].url)
       res.locals.imgURL = results[0].url 
-      return next();
+      next();
     })
     .catch(err => {
       console.log(err);
       })
+}
+
+//getting all the items & their respective images 
+itemController.getItems = (req, res, next) => {
+  //send a url 
 }
 
 
--- server/server.js
+++ server/server.js
@@ -6,25 +6,32 @@ const app = express();
 const PORT = 3000;
 const cloudinary = require('cloudinary').v2;
 const formData = require('express-form-data')
+// const fileupload = require('express-fileupload');
 const itemController = require('./controllers/itemController');
-
-// cloudinary.config({ 
-//   cloud_name: 'swapme', 
-//   api_key: '664311791237882', 
-//   api_secret: 'QSaAUgEMAaUhC-Jl5q6oIqk9SYI' 
-// });
+const userController = require('./controllers/userController')
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(formData.parse())
+// app.use(fileupload({useTempFiles:true}));
+
+
+//serve the stylesheet and the index.js
 
 
 //post request to add images 
-app.post('/uploadImage', itemController.addImage, (req, res) =>{
+app.post('/uploadImage', itemController.addImage, 
+// itemController.getUserId, 
+// itemController.saveImage, 
+(req, res) =>{
   res.status(200).json(res.locals.imgURL);
 })
 
+//get request 
+app.get('/items', itemController.getItems, (req, res) =>{
+  res.status(200)
+})
 
 
 
