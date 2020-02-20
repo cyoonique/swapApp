7beff576a6d7ca9a26f3aa750fdeca80d8f1ac84
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-02-19 17:14:51 -0800

    Login Page created with toggle

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions/actions.js";
 
+import LoginPage from "../components/loginPage.jsx";
 import topNavBar from "../components/topNavBar.jsx";
 // import SwapContainer from "./SwapContainer.jsx";
 
@@ -18,17 +19,28 @@ const mapDispatchToProps = dispatch => ({
 
 class MainContainer extends Component {
     render(props) {
+
+        let loginPage;
+        if (this.props.loginDisplayTog === true) {
+            loginPage = (
+                <LoginPage
+                        loginDisplayToggle={this.props.loginDisplayToggle}
+                        loginDisplayTog={this.props.loginDisplayTog}
+                     />
+            )
+        }
         return (
             <main>
                 <div id="main">
+                    <div className="loginPage"> {loginPage} </div>
 
-                    <button id="test" onClick={() => { 
+                    {/* <button id="test" onClick={() => { 
                         console.log('I got clicked') 
                         this.props.loginDisplayToggle();
                         console.log(this.props.loginDisplayTog);
                         }} > 
                     test me!
-                    </button>
+                    </button> */}
 
                     
                     <topNavBar />
