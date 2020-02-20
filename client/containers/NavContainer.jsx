b669cedd92dff2560df612659e7e016b43936753
cyoonique <choi.yoon333@gmail.com>
2020-02-19 16:44:09 -0800

    create navbar

--- client/containers/NavContainer.jsx
+++ client/containers/NavContainer.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
-                <Link to='/' className="brand-logo">SwapThing</Link>
+                <a className="brand-logo">SwapThing</a>
+                <ul className="right">
+                    <li><Link to="/feed">Feed</Link></li>
+                    <li><Link to="/item">User Item</Link></li>
+                    <li><Link to="/barter">Barter</Link></li>
+                    <li><Link to="/upload">Upload</Link></li>
+                </ul>
             </div>
         </nav>
     )
